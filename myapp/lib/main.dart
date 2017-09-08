0fa2c34b71439a21263eb3bf11068f527231b3db
zhouzhihui <zhouzhihui@htgames.cn>
2017-09-08 18:03:26 +0800

    add some code

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -28,56 +28,55 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<ChatMessage> _message = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
-
+  int _counter = 0;
   Widget _buildTextComposer() {
     return new IconTheme(
         data: new IconThemeData(color: Theme.of(context).accentColor),
         child: new Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new Row(
-          children: <Widget>[
-            new Flexible(
-              child: new TextField(
-                controller: _textController,
-                onSubmitted: _handleSubmmited,
-                decoration: new InputDecoration.collapsed(
-                    hintText: "Send a message"),
-              ),
-            ),
-            new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(icon: new Icon(Icons.send),
-//                  onPressed: () => _handleSubmmited(_textController.text)),
-                  onPressed: () {
-                    return _handleSubmmited(_textController.text);
-                  }),
-            ),
-          ],
-        )
-    ));
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(
+                  child: new TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmmited,
+                    decoration: new InputDecoration.collapsed(
+                        hintText: "Send a message"),
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                  child: new IconButton(
+                      icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmmited(_textController.text)),
+                ),
+              ],
+            )));
   }
 
   void _handleSubmmited(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _message.insert(0, message);
+    });
   }
 
   void _incrementCounter() {
@@ -144,33 +143,63 @@ class _MyHomePageState extends State<MyHomePage> {
 //                );
               },
               color: Colors.blue[400],
-              child: new Text(
-                  'RaisedButton', style: new TextStyle(color: Colors.white)),
+              child: new Text('RaisedButton',
+                  style: new TextStyle(color: Colors.white)),
             ),
             new FlatButton(
-              onPressed: (){},
+              onPressed: () {},
               color: Colors.blue[400],
-              child: new Text('FlatButton',style: new TextStyle(color: Colors.white)),
+              child: new Text('FlatButton',
+                  style: new TextStyle(color: Colors.white)),
             ),
             _buildTextComposer(),
           ],
         ),
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
 //        child: new Icon(Icons.add)
-        child: new Text(
+          child: new Text(
             "add",
-        )
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
+const String _name = "Zhihui Zhou";
+
 class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
   @override
   Widget build(BuildContext context) {
-    return null;
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(
+              child: new Text(_name[0]),
+            ),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(
+                _name,
+                style: Theme.of(context).textTheme.subhead,
+              ),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
